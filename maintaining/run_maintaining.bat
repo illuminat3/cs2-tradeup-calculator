4cbc2d599efe0a36a38f071e8ad1d3e529a75336
illuminat3 <matthewrawson2@gmail.com>
2026-03-09 13:53:49 +0000

    update script to allow for output to be seen

--- maintaining/run_maintaining.bat
+++ maintaining/run_maintaining.bat
@@ -8,24 +8,26 @@ if not exist "data\" mkdir data
 echo Running maintaining scripts
 echo.
 
-set FAILED=0
+set "FAILED=0"
 
 for %%f in (maintaining\*.py) do (
     echo Running %%f...
-    set PYTHONPATH=%CD%
+    set "PYTHONPATH=%CD%"
     python "%%f"
     if !errorlevel! neq 0 (
+        echo.
         echo %%f failed.
-        set FAILED=1
+        set "FAILED=1"
+        echo Press any key to close...
+        pause >nul
+        exit /b 1
     ) else (
+	echo.
         echo %%f completed.
+	pause >nul
     )
     echo.
 )
 
-if %FAILED%==1 (
-    echo One or more scripts failed.
-    exit /b 1
-) else (
-    echo All scripts completed successfully.
-)
+echo All scripts completed successfully.
+exit /b 0
