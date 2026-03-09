6d78966dbedcbe94b99dee24d37ec5e6573dea40
illuminat3 <matthewrawson2@gmail.com>
2026-03-09 22:11:05 +0000

    add implementation

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from services.search_service import search_service
 from services.input_analyser_service import input_analyser_service
 from services.skin_repository import skin_repository
 from services.marketplaces.csfloat.csfloat_marketplace import csfloat_marketplace
+from services.skin_selector_service import skin_selector_service
 
 search_file = "config/search.json"
 config_file = "config/marketplace-config.json"
@@ -15,19 +16,24 @@ def main():
 	skin_repo = skin_repository(skins_file)
 	input_analyser_svc = input_analyser_service(skin_repo)
 	csfloat_marketplace_svc = csfloat_marketplace(csfloat_config.api_key, csfloat_config.base_url)
+	skin_selector_svc = skin_selector_service()
 
 	search_settings = search_svc.get_search_settings()
 	inputs = input_analyser_svc.analyse_input(search_settings)
 
+	listing_inputs = {}
 	for count, skins in inputs.items():
+		total_listings = []
 		for skin in skins:
 			listings = csfloat_marketplace_svc.find_listings(skin)
-			if listings:
-				print(f"Found {len(listings)} listings for {count}x {skin.collection_name} | {skin.finish_name} {skin.weapon_type.name}:")
-				for listing in listings:
-					print(f"  - Price: ${listing.price:.2f}, Float: {listing.skin.float_value:.4f}, Link: {listing.url}")
-			else:
-				print(f"No listings found for {count}x {skin.collection_name} | {skin.finish_name} {skin.weapon_type.name}")
+			total_listings.extend(listings)
+
+		listing_inputs[count] = total_listings
+	
+	selected_listings = skin_selector_svc.select_skins_for_tradeup(listing_inputs, search_settings.target_float)
+
+	for listing in selected_listings:
+		print(f"Selected: {listing.skin.skin_name} | Price: ${listing.price:.2f}, Float: {listing.skin.float_value:.4f}, Link: {listing.url}")
 
 if __name__ == "__main__":
 	main()
